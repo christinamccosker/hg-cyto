50d8d95ac1311014a7e1f7fa05db3f72d0771514
christinamccosker <christina.mccosker@maine.edu>
2024-08-05 12:54:48 -0400

    update

--- _build.sh
+++ _build.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env Rscript
+#!/bin/sh
 
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+set -ev
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
--- _deploy.sh
+++ _deploy.sh
@@ -1,3 +1,16 @@
-#!/usr/bin/env Rscript
+#!/bin/sh
 
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "christina.mccosker@maine.edu"
+git config --global user.name "christinamccosker"
+
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin docs
